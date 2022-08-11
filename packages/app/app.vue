09f5fb3b40a696180bafa6de420be206a340d0f3
Pooya Parsa <pooya@pi0.io>
2022-08-11 18:53:41 +0200

    update monorepo and enable viteNode

--- packages/app/app.vue
+++ packages/app/app.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome /> {{id}}
+    Nuxt app! {{ id }}
   </div>
 </template>
 
 <script lang="ts" setup>
-
 import {v4} from "uuid"
-
 
 const id = v4()
 </script>
